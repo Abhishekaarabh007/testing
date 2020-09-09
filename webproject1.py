cd9859a7900f1f44898f56fb7d9bebe969e82b26
Abhishekaarabh007 <66726545+Abhishekaarabh007@users.noreply.github.com>
2020-09-09 16:53:13 +0530

    Add files via upload

---
+++ webproject1.py
@@ -0,0 +1,37 @@
+import simplexml
+from flask import request,make_response, Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app, default_mediatype='application/json')
+
+@api.representation('application/xml')
+def xml(data, code, headers=None):
+    resp = make_response(simplexml.core.dumps({'response': data}), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+
+
+class dump(Resource):
+    def get(self):
+        attrib=request.args.get('attrib1',type=int)
+        return {'result':attrib**2}
+      
+    
+   
+
+
+api.add_resource(dump, '/square') 
+
+
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
