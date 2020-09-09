6c137fcda8a151515527a6aa48f82abea50f5156
Abhishekaarabh007 <66726545+Abhishekaarabh007@users.noreply.github.com>
2020-09-09 17:25:36 +0530

    Update webproject1.py

--- webproject1.py
+++ webproject1.py
@@ -27,11 +27,3 @@ api.add_resource(dump, '/square')
 
 
 
-if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
